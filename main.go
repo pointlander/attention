be711d94b48faf4067b3037e5ea1eab719e0276e
Andrew Snodgrass <git@pointlander.info>
2022-05-04 18:25:49 -0500

    Added iris autoencoder model

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"gonum.org/v1/plot/vg"
 	"gonum.org/v1/plot/vg/draw"
 
+	"github.com/pointlander/datum/iris"
 	"github.com/pointlander/gradient/tf64"
 )
 
@@ -23,6 +24,10 @@ func main() {
 	Regular(1, false)
 	Attention(1, true)
 	Regular(1, true)
+	IRISAttention(1, false)
+	IRISAttention(1, true)
+	IRISRegular(1, false)
+	IRISRegular(1, true)
 
 	attention := 0.0
 	for i := 0; i < 128; i++ {
@@ -32,6 +37,14 @@ func main() {
 	for i := 0; i < 128; i++ {
 		attentionFFT += float64(Attention(int64(2+i), true))
 	}
+	irisAttention := 0.0
+	for i := 0; i < 128; i++ {
+		irisAttention += float64(IRISAttention(int64(2+i), false))
+	}
+	irisAttentionFFT := 0.0
+	for i := 0; i < 128; i++ {
+		irisAttentionFFT += float64(IRISAttention(int64(2+i), true))
+	}
 	regular := 0.0
 	for i := 0; i < 128; i++ {
 		regular += float64(Regular(int64(2+i), false))
@@ -40,10 +53,22 @@ func main() {
 	for i := 0; i < 128; i++ {
 		regularFFT += float64(Regular(int64(2+i), true))
 	}
+	irisRegular := 0.0
+	for i := 0; i < 128; i++ {
+		irisRegular += float64(IRISRegular(int64(2+i), false))
+	}
+	irisRegularFFT := 0.0
+	for i := 0; i < 128; i++ {
+		irisRegularFFT += float64(IRISRegular(int64(2+i), true))
+	}
 	fmt.Println("Attention:", attention/128)
 	fmt.Println("Attention FFT:", attentionFFT/128)
+	fmt.Println("IRIS Attention:", irisAttention/128)
+	fmt.Println("IRIS Attention FFT:", irisAttentionFFT/128)
 	fmt.Println("Regular:", regular/128)
 	fmt.Println("Regular FFT:", regularFFT/128)
+	fmt.Println("IRIS Regular:", irisRegular/128)
+	fmt.Println("IRIS Regular FFT:", irisRegularFFT/128)
 }
 
 // MakeLoops make worldline loops
@@ -144,7 +169,7 @@ func Attention(seed int64, fft bool) int {
 
 		total += tf64.Gradient(cost).X[0]
 		sum := 0.0
-		for _, p := range set.Weights[1:] {
+		for _, p := range set.Weights {
 			for _, d := range p.D {
 				sum += d * d
 			}
@@ -195,6 +220,135 @@ func Attention(seed int64, fft bool) int {
 		filename := "cost_attention.png"
 		if fft {
 			filename = "cost_attention_fft.png"
+		}
+		err = p.Save(8*vg.Inch, 8*vg.Inch, filename)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return i
+}
+
+// IRISAttentino is an attention base neural network for the iris dataset
+func IRISAttention(seed int64, fft bool) int {
+	rnd := rand.New(rand.NewSource(seed))
+
+	datum, err := iris.Load()
+	if err != nil {
+		panic(err)
+	}
+	fisher := datum.Fisher
+
+	others := tf64.NewSet()
+	others.Add("data", 4, len(fisher))
+
+	w := others.Weights[0]
+	for _, data := range fisher {
+		w.X = append(w.X, data.Measures...)
+	}
+
+	set := tf64.NewSet()
+	set.Add("aw", 4, 8)
+	set.Add("bw", 4, 8)
+	set.Add("cw", 8, 4)
+	set.Add("ab", 8, 1)
+	set.Add("bb", 8, 1)
+	set.Add("cb", 4, 1)
+
+	if fft {
+		for _, w := range set.Weights[:3] {
+			factor := math.Sqrt(1/float64(cap(w.X))) * math.Sqrt(2.0/float64(w.S[0]))
+			weights, weight := MakeWeightsFFT(rnd, cap(w.X)), 0
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, weights[weight]*factor)
+				weight++
+			}
+		}
+	} else {
+		for _, w := range set.Weights[:3] {
+			factor := math.Sqrt(2.0 / float64(w.S[0]))
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, rnd.NormFloat64()*factor)
+			}
+		}
+	}
+
+	for i := 3; i < len(set.Weights); i++ {
+		set.Weights[i].X = set.Weights[i].X[:cap(set.Weights[i].X)]
+	}
+
+	deltas := make([][]float64, 0, 8)
+	for _, p := range set.Weights {
+		deltas = append(deltas, make([]float64, len(p.X)))
+	}
+
+	la := tf64.Add(tf64.Mul(set.Get("aw"), others.Get("data")), set.Get("ab"))
+	lb := tf64.Add(tf64.Mul(set.Get("bw"), others.Get("data")), set.Get("bb"))
+	lc := tf64.Hadamard(la, lb)
+	f := tf64.Add(tf64.Mul(set.Get("cw"), lc), set.Get("cb"))
+	cost := tf64.Avg(tf64.Quadratic(f, others.Get("data")))
+
+	alpha, eta, iterations := .3, .3, 1024
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		total := 0.0
+		set.Zero()
+
+		total += tf64.Gradient(cost).X[0]
+		sum := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				sum += d * d
+			}
+		}
+		norm := math.Sqrt(sum)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		for j, w := range set.Weights {
+			for k, d := range w.D {
+				deltas[j][k] = alpha*deltas[j][k] - eta*d*scaling
+				set.Weights[j].X[k] += deltas[j][k]
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: total})
+		if seed == 1 {
+			fmt.Println(i, total)
+		}
+		if total < 5 {
+			break
+		}
+		i++
+	}
+
+	if seed == 1 {
+		f(func(a *tf64.V) bool {
+			fmt.Println(i, a.X)
+			return true
+		})
+
+		p := plot.New()
+
+		p.Title.Text = "epochs vs cost"
+		p.X.Label.Text = "epochs"
+		p.Y.Label.Text = "cost"
+
+		scatter, err := plotter.NewScatter(points)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(1)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		filename := "cost_iris_attention.png"
+		if fft {
+			filename = "cost_iris_attention_fft.png"
 		}
 		err = p.Save(8*vg.Inch, 8*vg.Inch, filename)
 		if err != nil {
@@ -268,7 +422,7 @@ func Regular(seed int64, fft bool) int {
 
 		total += tf64.Gradient(cost).X[0]
 		sum := 0.0
-		for _, p := range set.Weights[1:] {
+		for _, p := range set.Weights {
 			for _, d := range p.D {
 				sum += d * d
 			}
@@ -319,6 +473,131 @@ func Regular(seed int64, fft bool) int {
 		filename := "cost_regular.png"
 		if fft {
 			filename = "cost_regular_fft.png"
+		}
+		err = p.Save(8*vg.Inch, 8*vg.Inch, filename)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return i
+}
+
+// IRISRegular is a regular neural network trained on the iris data set
+func IRISRegular(seed int64, fft bool) int {
+	rnd := rand.New(rand.NewSource(seed))
+
+	datum, err := iris.Load()
+	if err != nil {
+		panic(err)
+	}
+	fisher := datum.Fisher
+
+	others := tf64.NewSet()
+	others.Add("data", 4, len(fisher))
+
+	w := others.Weights[0]
+	for _, data := range fisher {
+		w.X = append(w.X, data.Measures...)
+	}
+
+	set := tf64.NewSet()
+	set.Add("aw", 4, 4)
+	set.Add("bw", 4, 4)
+	set.Add("ab", 4, 1)
+	set.Add("bb", 4, 1)
+
+	if fft {
+		for _, w := range set.Weights[:2] {
+			factor := math.Sqrt(1/float64(cap(w.X))) * math.Sqrt(2.0/float64(w.S[0]))
+			weights, weight := MakeWeightsFFT(rnd, cap(w.X)), 0
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, weights[weight]*factor)
+				weight++
+			}
+		}
+	} else {
+		for _, w := range set.Weights[:2] {
+			factor := math.Sqrt(2.0 / float64(w.S[0]))
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, rnd.NormFloat64()*factor)
+			}
+		}
+	}
+
+	for i := 2; i < len(set.Weights); i++ {
+		set.Weights[i].X = set.Weights[i].X[:cap(set.Weights[i].X)]
+	}
+
+	deltas := make([][]float64, 0, 8)
+	for _, p := range set.Weights {
+		deltas = append(deltas, make([]float64, len(p.X)))
+	}
+
+	l1 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("aw"), others.Get("data")), set.Get("ab")))
+	l2 := tf64.Add(tf64.Mul(set.Get("bw"), l1), set.Get("bb"))
+	cost := tf64.Avg(tf64.Quadratic(l2, others.Get("data")))
+
+	alpha, eta, iterations := .3, .3, 1024
+	points := make(plotter.XYs, 0, iterations)
+	i := 0
+	for i < iterations {
+		total := 0.0
+		set.Zero()
+
+		total += tf64.Gradient(cost).X[0]
+		sum := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				sum += d * d
+			}
+		}
+		norm := math.Sqrt(sum)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+
+		for j, w := range set.Weights {
+			for k, d := range w.D {
+				deltas[j][k] = alpha*deltas[j][k] - eta*d*scaling
+				set.Weights[j].X[k] += deltas[j][k]
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: total})
+		if seed == 1 {
+			fmt.Println(i, total)
+		}
+		if total < 5 {
+			break
+		}
+		i++
+	}
+
+	if seed == 1 {
+		l2(func(a *tf64.V) bool {
+			fmt.Println(i, a.X)
+			return true
+		})
+
+		p := plot.New()
+
+		p.Title.Text = "epochs vs cost"
+		p.X.Label.Text = "epochs"
+		p.Y.Label.Text = "cost"
+
+		scatter, err := plotter.NewScatter(points)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(1)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		filename := "cost_iris_regular.png"
+		if fft {
+			filename = "cost_iris_regular_fft.png"
 		}
 		err = p.Save(8*vg.Inch, 8*vg.Inch, filename)
 		if err != nil {
